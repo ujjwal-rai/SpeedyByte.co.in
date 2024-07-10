dcdcd494c4fd3e641f2d8e98c17e467f629f08e3
Ujjwal Rai <96328364+ujjwal-rai@users.noreply.github.com>
2024-07-10 21:36:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,36 +14,7 @@ function myFunction() {
   }
 }
 
-function myFunction2() {
-  var dots = document.getElementById("dots2");
-  var moreText = document.getElementById("more2");
-  var btnText = document.getElementById("myBtn2");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read More";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read Less";
-    moreText.style.display = "inline";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  const imageContainer = document.querySelector(".home-tint1");
-  const homeImage = document.querySelector(".home-image300");
-
-  imageContainer.addEventListener("mouseover", () => {
-    homeImage.style.display = "inline";
-  });
-
-  imageContainer.addEventListener("mouseout", () => {
-    homeImage.style.display = "none";
-  });
-});
-
-//
 document.addEventListener("DOMContentLoaded", function () {
   var myButton = document.getElementById("myButton");
   var myPopup = document.getElementById("myPopup");
